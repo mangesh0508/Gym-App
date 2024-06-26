32b37dcd5d6537c2efe4ca9bba12edb11bf96324
Mangesh Tanaji Dalvi <112159984+mangesh0508@users.noreply.github.com>
2024-06-26 16:50:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = requirs('cors')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
